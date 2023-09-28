c727a4862540db93000baabf634ab62917ac7f57
Oleksa <opetriw@gmail.com>
2023-09-27 22:43:29 -0600

    Finished BMI practice example

--- CPSC1517Workbook/ObjectClassLibraryTest/BodyMassIndexTest.cs
+++ CPSC1517Workbook/ObjectClassLibraryTest/BodyMassIndexTest.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using ObjectClassLibrary;
-using Xunit.Sdk;
 
 namespace ObjectClassLibraryTest
 {
@@ -21,7 +20,7 @@ namespace ObjectClassLibraryTest
         public void BodyMassIndex_Bmi_ReturnsCorrectBMIValue()
         {
             //Arrange
-            BodyMassIndex instance = GenerateBodyMassIndexInstance(); ;
+            BodyMassIndex instance = GenerateBodyMassIndexInstance();
             double actual;
 
             //Act
@@ -48,7 +47,7 @@ namespace ObjectClassLibraryTest
         [Theory]
         [InlineData("", Weight, Height)]
         [InlineData("  ", Weight, Height)]
-        public void BodyMassIndex_BodyMassIndex_ReturnsArgumentNullExceptionForBlankName(string name, double weight, double height)
+        public void BodyMassIndex_Constructor_ThrowsForBlankName(string name, double weight, double height)
         {
             //Arrange
             BodyMassIndex instance;
@@ -58,6 +57,116 @@ namespace ObjectClassLibraryTest
 
             //Assert
             act.Should().Throw<ArgumentNullException>().WithMessage("Name cannot be blank");
+        }
+
+        [Theory]
+        [InlineData(Name, 0, Height)]
+        [InlineData(Name, -100, Height)]
+        public void BodyMassIndex_Constructor_ThrowsForNonPositiveWeight(string name, double weight, double height)
+        {
+            //Arrange
+            BodyMassIndex instance;
+
+            //Act
+            Action act = () => instance = new BodyMassIndex(name, weight, height);
+
+            //Assert
+            act.Should().Throw<ArgumentOutOfRangeException>().WithMessage("Weight must be a positive non-zero number");
+        }
+
+        [Theory]
+        [InlineData(Name, Weight, 0)]
+        [InlineData(Name, Weight, -100)]
+        public void BodyMassIndex_Constructor_ThrowsForNonPositiveHeight(string name, double weight, double height)
+        {
+            //Arrange
+            BodyMassIndex instance;
+
+            //Act
+            Action act = () => instance = new BodyMassIndex(name, weight, height);
+
+            //Assert
+            act.Should().Throw<ArgumentOutOfRangeException>().WithMessage("Height must be a positive non-zero number");
+        }
+
+        [Theory]
+        [InlineData("Underweight Person1", 90, 60, 17.6, "underweight")]
+        [InlineData("Underweight Person2", 120, 75, 15.0, "underweight")]
+        public void BodyMassIndex_BmiCategory_ReturnsUnderweightCategory(string name, double weight, double height, double expectedBMI, string expectedCategory)
+        {
+            //Arrange
+            BodyMassIndex instance;
+            string actualCategory;
+            double actualBmi;
+
+            //Act
+            instance = new BodyMassIndex(name, weight, height);
+            actualCategory = instance.BmiCategory();
+            actualBmi = instance.Bmi();
+
+            //Assert
+            actualCategory.Should().Be(expectedCategory);
+            actualBmi.Should().Be(expectedBMI);
+        }
+
+        [Theory]
+        [InlineData("Normal weight Person1", 111, 65, 18.5, "normal")]
+        [InlineData("Normal weight Person2", 149, 65, 24.8, "normal")]
+        public void BodyMassIndex_BmiCategory_ReturnsNormalCategory(string name, double weight, double height, double expectedBMI, string expectedCategory)
+        {
+            //Arrange
+            BodyMassIndex instance;
+            string actualCategory;
+            double actualBmi;
+
+            //Act
+            instance = new BodyMassIndex(name, weight, height);
+            actualCategory = instance.BmiCategory();
+            actualBmi = instance.Bmi();
+
+            //Assert
+            actualCategory.Should().Be(expectedCategory);
+            actualBmi.Should().Be(expectedBMI);
+        }
+
+        [Theory]
+        [InlineData("Overweight Person1", 150, 65, 25.0, "overweight")]
+        [InlineData("Overweight Person2", 179, 65, 29.8, "overweight")]
+        public void BodyMassIndex_BmiCategory_ReturnsOverweightCategory(string name, double weight, double height, double expectedBMI, string expectedCategory)
+        {
+            //Arrange
+            BodyMassIndex instance;
+            string actualCategory;
+            double actualBmi;
+
+            //Act
+            instance = new BodyMassIndex(name, weight, height);
+            actualCategory = instance.BmiCategory();
+            actualBmi = instance.Bmi();
+
+            //Assert
+            actualCategory.Should().Be(expectedCategory);
+            actualBmi.Should().Be(expectedBMI);
+        }
+
+        [Theory]
+        [InlineData("Obese Person1", 180, 65, 30.0, "obese")]
+        [InlineData("Obese Person2", 210, 65, 34.9, "obese")]
+        public void BodyMassIndex_BmiCategory_ReturnsObeseCategory(string name, double weight, double height, double expectedBMI, string expectedCategory)
+        {
+            //Arrange
+            BodyMassIndex instance;
+            string actualCategory;
+            double actualBmi;
+
+            //Act
+            instance = new BodyMassIndex(name, weight, height);
+            actualCategory = instance.BmiCategory();
+            actualBmi = instance.Bmi();
+
+            //Assert
+            actualCategory.Should().Be(expectedCategory);
+            actualBmi.Should().Be(expectedBMI);
         }
     }
 }
--- ObjectClassLibrary/BodyMassIndex.cs
+++ ObjectClassLibrary/BodyMassIndex.cs
@@ -9,28 +9,40 @@
         {
             get
             {
-                return _height;
+                // return _height;
+                // Problem Description: returned incorrect field (_height), changed to correct field (_weight).
+                return _weight;
             }
             set
             {
                 if (value <= 0)
                 {
-                    throw new ArgumentNullException("Weight must be a positive non-zero value");
+                    // throw new ArgumentNullException("Weight must be a positive non-zero value");
+                    // corrected to out of range exception and message
+
+                    throw new ArgumentOutOfRangeException("Weight must be a positive non-zero number", new ArgumentException());
                 }
-                Height = value;
+                // Height = value;
+                // Problem Description: assigned value to Height property, corrected to _weight field.
+                _weight = value;
             }
         }
         public double Height
         {
             get
             {
-                return Height;
+                // return Height;
+                // Problem Description: returned Height property, corrected to _height field.
+                return _height;
             }
             set
             {
                 if (value <= 0)
                 {
-                    throw new ArgumentException("Height must be a positive non-zero value");
+                    // throw new ArgumentException("Height must be a positive non-zero value");
+                    // corrected to out of range exception and message
+
+                    throw new ArgumentOutOfRangeException("Height must be a positive non-zero number", new ArgumentException());
                 }
                 _height = value;
             }
@@ -39,14 +51,25 @@
         {
             if (string.IsNullOrWhiteSpace(name))
             {
-                name = name;
+                // name = name;
+                // Problem Description: assigned value to itself (stack overflow)
+                // Corrected to throw exception if string is null or empty or whitespace
+                // also, corrected to assign name argument to Name property
+                // also, changed to argument null exception
+                throw new ArgumentNullException("Name cannot be blank", new ArgumentException());
             }
-            else
-            {
-                throw new ArgumentException("Name cannot be blank");
-            }
+
+            Name = name;
+            //else
+            //{
+            //throw new ArgumentException("Name cannot be blank");
+            //}
+
             this.Weight = weight;
-            height = this.Height;
+            // height = this.Height;
+            // Problem Description: was assigning null height to the height argument
+            // Corrected to assign height argument to Height property
+            Height = height;
         }
         /// <summary>
         /// Calculate the body mass index (BMI) using the weight and height of the person.
@@ -57,7 +80,10 @@
         /// <returns>the body mass index (BMI) value of the person</returns>
         public double Bmi()
         {
-            double bmiValue = 703 * Weight / Math.Pow(2, Height);
+            // double bmiValue = 703 * Weight / Math.Pow(2, Height);
+            // Problem Description: summary comments are incorrect. Coefficient is 703, not 700.
+            // incorrect Math.Pow usage (should be Math.Pow(Height, 2))
+            double bmiValue = 703 * _weight / Math.Pow(_height, 2);
             bmiValue = Math.Round(bmiValue, 1);
             return bmiValue;
         }
@@ -69,19 +95,25 @@
         {
             string category = "Unknown";
             double bmiValue = Bmi();
+            // Problem Description: consecutive if statements overwrite lesser categories due to overlapping ranges
+            // i.e. every underweight and normal value will be overwritten as overweight due to non-branching logic
+            // solution: 
             if (bmiValue < 18.5)
             {
                 category = "underweight";
             }
-            if (bmiValue < 24.9)
+            //if (bmiValue < 24.9)
+            else if (bmiValue < 24.9)
             {
                 category = "normal";
             }
-            if (bmiValue < 29.9)
+            //if (bmiValue < 29.9)
+            else if (bmiValue < 29.9)
             {
                 category = "overweight";
             }
-            if (bmiValue >= 30)
+            //if (bmiValue >= 30)
+            else if (bmiValue >= 30)
             {
                 category = "obese";
             }
