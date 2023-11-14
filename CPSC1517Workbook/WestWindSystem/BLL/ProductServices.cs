5a8b05400f97cae8827bfc538ff78882efcdec33
Oleksa <opetriw@gmail.com>
2023-11-13 20:35:19 -0700

    updates to search

--- CPSC1517Workbook/WestWindSystem/BLL/ProductServices.cs
+++ CPSC1517Workbook/WestWindSystem/BLL/ProductServices.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,7 +26,27 @@ namespace WestWindSystem.BLL
         /// <returns>List of proucts for a category id</returns>
         public List<Product> GetProductsByCategoryId(int id)
         {
-            return _context.Products.Where(p => p.CategoryId == id).ToList<Product>();
+            return _context.Products.Where(p => p.CategoryId == id)
+                .Include(p => p.Supplier)
+                .ToList<Product>();
+        }
+
+        /*public List<Product> GetProductsByNameOrCategoryName(string partial )
+        {
+            partial = partial.ToLower();
+            return _context.Products.Where(p => p.ProductName.ToLower().Contains(partial)
+            || p.Category.CategoryName.ToLower().Contains(partial))
+                .Include(p => p.Supplier)
+                .ToList<Product>();
+        }*/
+
+        public List<Product> GetProductsByNameOrSupplierName(string partial)
+        {
+            partial = partial.ToLower();
+            return _context.Products.Where(p => p.ProductName.ToLower().Contains(partial)
+            || p.Supplier.CompanyName.ToLower().Contains(partial))
+                .Include(p => p.Supplier)
+                .ToList<Product>();
         }
     }
 }
